77505db687397d9cf9f252f0388717da99618716
k1arac <90221309+k1arac@users.noreply.github.com>
2022-06-09 19:13:44 -0400

    first commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
+
+import "./About.css";
+import kiara_photo from "../assets/kiara_photo.jpg";
+
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        
+        <div class="split left">
+          <div className="centered">
+            <img className="profile_image"
+            src={kiara_photo} alt = "Profile pic" 
+            ></img>  
+          </div>
+        </div>
+
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Kiara </div>
+            <div className="brief_description">
+              <ul>
+              <li>Hi! I'm Kiara and I'm from Long Island, New York. </li>
+
+              Interests:
+              <li>I like working out!</li>
+              <li>I enjoy reading books and listening to music</li>
+
+              Fun fact: 
+              <li>I have a boston terrier and his name is Milo!</li> 
+              </ul>
+            </div>
+          </div>
+        </div> 
       </div>
+
     )
   }
 }
